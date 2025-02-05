9da08a9fab3c13e00d0c8d2a5f056c3949d2178d
meheraz1100 <meheraz1100@gmail.com>
2025-02-05 21:39:11 +0600

    added react modal

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SERVER_URL = "chat-app-v2-server-production.up.railway.app"; // Replace with your actual backend URL
+const SERVER_URL = "http://localhost:4000/"; // Replace with your actual backend URL
 
 export const socket = io(SERVER_URL, {
     transports: ["websocket"],
